4b725ab3765630b9f03385e6ac008b9939840ef9
Christoph Moeller <christoph.moeller@disroot.org>
2022-05-29 00:19:29 +0900

    add new cont, view, route manually. Change  app/views/layouts/application.html.erb (layout for all pages)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  # get 'home/index' (default when creating controller)
+  root 'home#index'
+  get 'home/about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
